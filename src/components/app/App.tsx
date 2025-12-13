75f9ba6d1b015b28ac0b0f12b2f508a89129a924
Garrett <garrett.rose03@gmail.com>
2025-12-13 19:00:39 +0200

    State/ local storage for today date logged in grid

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,12 @@
 import '@/styles/index.css'
 import Grid from '@/components/grid/Grid.tsx'
-import Form from '@/components/form/Form.tsx'
+// import Form from '@/components/form/Form.tsx'
 
 function App() {
 
   return (
     <>
       <Grid/>
-      <Form/>
     </>
   )
 }
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,15 +1,50 @@
 import Boxes from '@/components/boxes/Boxes'
 import { setDate } from "@/utils/dateRange.ts"
-
-const dateRange: Array<{ date: string, activity: boolean, level: number}> = setDate();
+import { useState } from "react"
 
 export default function Grid(){
+
+  // const [dateRange, setDateRange] = useState<Array<{ date: string; activity: boolean; level: number }>>(() => setDate());
+  const [dateRange, setDateRange] = useState<DateItem[]>(() => {
+    const saved = localStorage.getItem("dateRange");
+    return saved ? JSON.parse(saved) : setDate();
+  });
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const todayIndex = dateRange.length -1;
+    //Study below
+    if (todayIndex === -1) return;
+
+    const newDateRange = [...dateRange];              
+    newDateRange[todayIndex] = {                     
+      ...newDateRange[todayIndex],
+      activity: true
+    };
+
+    localStorage.setItem("dateStuff", JSON.stringify(newDateRange));
+
+    //I hate this hack fix it pls
+    const saved = localStorage.getItem("dateStuff");
+    if (saved) {
+      setDateRange(JSON.parse(saved));  
+      if (dateRange.length === 0) return;
+
+      const todayIndex = dateRange.length - 1;
+      const updatedRange = [...dateRange];
+      updatedRange[todayIndex] = { ...updatedRange[todayIndex], activity: true };
+
+      setDateRange(updatedRange);
+      localStorage.setItem("dateRange", JSON.stringify(updatedRange));
+    }
+  };
 
   const elements = [];
 
   for (let i = 0; i < dateRange.length; i++) {
     const item = dateRange[i];
-
+    
     const className =
       item.activity && item.level === 0 ? "box-item-lvl-0" :
       item.activity && item.level === 1 ? "box-item-lvl-1" :
@@ -20,7 +55,17 @@ export default function Grid(){
   }
 
   return (
-      <Boxes elements={elements}/>
+  <>
+  <Boxes elements={elements}/>
+    <form className="form" onSubmit={onSubmit}>
+      <div className="form-inputs"> 
+        <input className="form-input-fields" name="activity"/>
+        <input className="form-input-fields"/>
+
+        <button type="submit" className="submit-form">Log Activity</button>
+      </div> 
+    </form> 
+  </>
   ) ;
 }
 
