99eeabaed5ddd51e23b5e192e682af7dc13aae01
Garrett <garrett.rose03@gmail.com>
2025-12-17 10:00:31 +0200

    building deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import checker from 'vite-plugin-checker'
 
 export default defineConfig({
+  base: '/repo-name', 
   plugins: [react(), checker({ typescript: true })],
   resolve: {
     alias: {
