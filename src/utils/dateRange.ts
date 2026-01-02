bec7d31de7d14652631f2a6e359c4e32761cd214
Garrett <garrett.rose03@gmail.com>
2026-01-02 10:14:24 +0200

    changed dateRange function to account for rendering a symmetrical grid

--- src/utils/dateRange.ts
+++ src/utils/dateRange.ts
@@ -1,21 +1,36 @@
-import { parse, format, addDays, isBefore } from 'date-fns';
+import {
+  format,
+  addDays,
+  startOfYear,
+  endOfYear,
+  startOfWeek,
+  endOfWeek,
+} from 'date-fns';
+
+export interface DateType {
+  date: string;
+  activity: boolean;
+  level: number;
+}
+
+type DateTypeArray = DateType[];
 
 export function setDate() {
-  const startDate = '2025-01-01';
-  const endDate = new Date();
+  const startDate = startOfYear(new Date());
+  const endDate = endOfYear(new Date());
 
-  let currentDate = parse(startDate, 'yyyy-MM-dd', new Date());
+  const gridStart = startOfWeek(startDate, { weekStartsOn: 1 });
+  const gridEnd = endOfWeek(endDate, { weekStartsOn: 1 });
 
-  const boundaryDate = endDate;
+  let dayToShow = gridStart;
 
-  const dateRange: Array<{ date: string; activity: boolean; level: number }> =
-    [];
+  const dateRange: DateTypeArray = [];
 
-  while (isBefore(currentDate, boundaryDate)) {
-    const dateKey = format(currentDate, 'yyyy-MM-dd');
+  while (dayToShow <= gridEnd) {
+    const dateKey = format(dayToShow, 'yyyy-MM-dd');
     dateRange.push({ date: dateKey, activity: false, level: 0 });
 
-    currentDate = addDays(currentDate, 1);
+    dayToShow = addDays(dayToShow, 1);
   }
 
   return dateRange;
