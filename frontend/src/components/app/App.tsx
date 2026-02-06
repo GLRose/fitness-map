14caca4a28a3520144ca8a03a193dc3a4597fc5d
Garrett <garrett.rose03@gmail.com>
2026-02-06 18:31:07 +0200

    packages added

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -3,7 +3,6 @@ import Links from '@/components/links/Links.tsx';
 import { Theme } from '@radix-ui/themes';
 import '@/styles/index.css';
 import '@/styles/links.css';
-
 import '@radix-ui/themes/styles.css';
 
 function App() {
